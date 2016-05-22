0e2a1a2c257fa43ade74f6c79fc248189c82632d
Alysson Tiago <autscc@gmail.com>
2016-05-22 14:03:27 -0300

    Interceptor loading pokeball.

--- app.modules.js
+++ app.modules.js
@@ -22,7 +22,48 @@
 	    'pokethon.homeController'
 	])
 
-	.config(function($stateProvider,$urlRouterProvider) {  
+	.factory('httpInterceptor', function($q,$rootScope) {
+  
+		  return {
+
+		    'request': function(config) {
+		    	$rootScope.$broadcast("loader_show");
+		      // do something on success
+		      return config || $q.when(config);
+		    },
+
+		    // optional method
+		   'requestError': function(rejection) {
+		      
+		      $rootScope.$broadcast("loader_hide");
+		      return $q.reject(rejection);
+		      
+		    },
+
+
+		    //Disparado ao receber a resposta da requisição.
+		    'response': function(response) {
+		       console.log('Carregado');
+		       $rootScope.$broadcast("loader_hide");
+		      // do something on success
+		      return response || $q.when(response);
+		    },
+
+		        // optional method
+		   'responseError': function(rejection) {
+		   		
+		   	  $rootScope.$broadcast("loader_hide");      
+		      return $q.reject(rejection);
+		    
+		    },
+
+		  };
+
+	})
+
+	.config(function($stateProvider,$urlRouterProvider,$httpProvider) {  
+
+	  $httpProvider.interceptors.push('httpInterceptor');
 
 	  $urlRouterProvider.otherwise("/");
 
@@ -35,7 +76,10 @@
 	    })
 	})
 
+
+
 	.run(['$rootScope', '$state', '$stateParams', function($rootScope, $state, $stateParams) {
+
 	    $rootScope.$state = $state;
 	    $rootScope.$stateParams = $stateParams;
 	    $state.go('index');
